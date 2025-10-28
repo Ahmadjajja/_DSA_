466f1e250f9578d9fce894f3173c51fcdebdedee
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-27 21:47:10 -0600

    Time: 58 ms (6.90%) | Memory: 19.2 MB (9.55%) - LeetSync

---
+++ tree/main/DailyLeetcoding/97-interleaving-string/interleaving-string.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        memo = {}
+
+        def dfs(i1, i2, i3, st):
+            print("s3 : ", s3)
+            print("st : ", st)
+            if i1 >= len(s1) and i2 >= len(s2):
+                return st == s3
+            if i3 >= len(s3):
+                return False
+            
+            if i1 < len(s1) and s3[i3] != s1[i1] and i2 < len(s2) and s3[i3] != s2[i2]:
+                return False
+            
+            if (i1, i2, i3) in memo:
+                return memo[(i1, i2, i3)]
+
+            left = False
+            if i1 < len(s1) and s3[i3] == s1[i1]:
+                left = dfs(i1 + 1, i2, i3 + 1, st + s3[i3])
+            right = False
+            if i2 < len(s2) and s3[i3] == s2[i2]:
+                right = dfs(i1, i2 + 1, i3 + 1, st + s3[i3])
+            
+            memo[(i1, i2, i3)] = left or right
+            
+            return left or right
+
+        return dfs(0, 0, 0, "")
+
+        
