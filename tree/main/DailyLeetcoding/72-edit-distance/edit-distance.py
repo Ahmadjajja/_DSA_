701522fd6b9f6427571b48685923b46ad0b89c92
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-28 21:56:24 -0600

    Time: 81 ms (15.72%) | Memory: 23.8 MB (7.57%) - LeetSync

---
+++ tree/main/DailyLeetcoding/72-edit-distance/edit-distance.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+# if s1[i-1] == s2[j-1]:
+#     dp[i][j] = dp[i-1][j-1]
+# else:
+    # dp[i][j] = 1 + min(
+    #     dp[i-1][j],    // Deletion
+    #     dp[i][j-1],    // Insertion
+    #     dp[i-1][j-1]   // Substitution
+    # )
+
+        matrix = [[0] * (len(word1) + 1) for _ in range(len(word2) + 1)]
+        for i in range(len(matrix)):
+            matrix[i][0] = i
+        for i in range(len(matrix[0])):
+            matrix[0][i] = i
+
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if word2[i - 1] == word1[j - 1]:
+                    matrix[i][j] = matrix[i-1][j-1]
+                else:
+                    matrix[i][j] = 1 + min(
+                        matrix[i-1][j],    # Deletion
+                        matrix[i][j-1],    # Insertion
+                        matrix[i-1][j-1]   # Substitution
+                    )        
+        print(matrix)
+
+        return matrix[len(matrix) - 1][len(matrix[0]) - 1]
+
+
+        
