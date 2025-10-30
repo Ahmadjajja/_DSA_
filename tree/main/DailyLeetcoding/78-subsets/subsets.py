48c58b80bf8926e9b297837ba86e9651679b9b44
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-29 21:29:39 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.34%) - LeetSync

--- tree/main/DailyLeetcoding/78-subsets/subsets.py
+++ tree/main/DailyLeetcoding/78-subsets/subsets.py
@@ -7,9 +7,7 @@ class Solution:
             if index >= len(nums):
                 res.append(subset.copy())
                 return
-            subset.append(nums[index])
-            dfs(index + 1, subset)
-            subset.pop()
+            dfs(index + 1, subset + [nums[index]])
             dfs(index + 1, subset)
         
         dfs(0, [])
