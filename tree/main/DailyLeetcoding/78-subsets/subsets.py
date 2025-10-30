79c49f3933a006a4840b66a490a8957780e6edbf
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-29 21:28:55 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (39.73%) - LeetSync

--- tree/main/DailyLeetcoding/78-subsets/subsets.py
+++ tree/main/DailyLeetcoding/78-subsets/subsets.py
@@ -2,25 +2,20 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-
-        subset = []
-
-        def DFS(index):
-
-            # base case
-            if index == len(nums):
+        def dfs(index, subset):
+            nonlocal res
+            if index >= len(nums):
                 res.append(subset.copy())
                 return
-            
-            # YES
             subset.append(nums[index])
-            DFS(index + 1)
-
-            # NO
+            dfs(index + 1, subset)
             subset.pop()
-            DFS(index + 1)
-
-        DFS(0)
+            dfs(index + 1, subset)
         
+        dfs(0, [])
+
         return res
+
+
+
         
