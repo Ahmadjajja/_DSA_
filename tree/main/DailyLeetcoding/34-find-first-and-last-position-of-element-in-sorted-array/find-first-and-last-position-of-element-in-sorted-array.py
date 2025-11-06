69da267f22cff8ae7d236f92a6214ea42cd5555e
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-11-05 22:02:44 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (52.61%) - LeetSync

--- tree/main/DailyLeetcoding/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ tree/main/DailyLeetcoding/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,27 +3,10 @@ class Solution:
         # there are 2 points:
         # 1. finding target is same as general bs
         start, end = -1, -1
-        l, r = 0, len(nums) - 1
-        targetIndex = -1
-
-        while l <= r:
-            mid = (l + r) // 2
-
-            if nums[mid] == target:
-                targetIndex = mid
-                break
-            elif nums[mid] > target:
-                r = mid - 1
-            else:
-                l = mid + 1
-
-        # print(nums[targetIndex] if targetIndex > -1 else -1)
-        if targetIndex == -1:
-            return [-1, -1]
 
         # 2. find starting pos
 
-        left, right = 0, targetIndex
+        left, right = 0, len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
@@ -31,12 +14,15 @@ class Solution:
             if nums[mid] == target:
                 start = mid
                 right = mid - 1
+
             elif nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
         # print("left, right -> ", left, right)
 
         # 2. find ending pos
-        left, right = targetIndex, len(nums) - 1
+        left, right = 0, len(nums) - 1
 
         while left <= right:
             mid = (left + right) // 2
@@ -45,6 +31,8 @@ class Solution:
                 left = mid + 1
             elif nums[mid] > target:
                 right = mid - 1
+            else:
+                left = mid + 1
         # print("left, right -> ", left, right)
 
         return [start, end]
