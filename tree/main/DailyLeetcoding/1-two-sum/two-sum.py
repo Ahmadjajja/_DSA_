d20fce6b422bedc5b17235d14481fea5e0fa950b
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-11-26 02:28:09 -0700

    Time: 3 ms (49.76%) | Memory: 18.8 MB (63.14%) - LeetSync

--- tree/main/DailyLeetcoding/1-two-sum/two-sum.py
+++ tree/main/DailyLeetcoding/1-two-sum/two-sum.py
@@ -1,50 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # # brute-force(will try to optimize later)
-        # for i in range(len(nums)):
-        #     for j in range(len(nums)):
-        #         if i == j:
-        #             continue
-        #         if nums[i] + nums[j] == target:
-        #             return [i, j]
-        
-        # return [-1, -1]
+        hashmap = {}  # value -> index
 
-        # # optimized approach
-        # # 2. TC: O(nlogn + n + n) = O(nlogn), SC: O(n)
-        # sortedNums = sorted(nums)
-        # num1, num2 = 0,  0
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in hashmap:
+                return [hashmap[comp], i]
+            hashmap[num] = i
 
-        # s, e = 0, len(nums) - 1
-        # while s <= e:
-
-        #     if sortedNums[s] + sortedNums[e] > target:
-        #         e -= 1
-        #     elif sortedNums[s] + sortedNums[e] < target:
-        #         s += 1
-        #     else:
-        #         num1 = sortedNums[s]
-        #         num2 = sortedNums[e]
-        #         break
-
-        # ans = []
-        # for i in range(len(nums)):
-        #     if nums[i] == num1 or nums[i] == num2:
-        #         ans.append(i)
-        #     if len(ans) == 2:
-        #         break
-        
-        # return ans
-
-        # even better approach
-        # 3. TC: O(n), SC: O(n)
-
-        hm = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if nums[i] in hm:
-                return [hm[nums[i]], i]
-            hm[diff] = i
-
-
-        
+        return [-1, -1]
