0ac9584f24a1d334d1f3d0877e913b29d7e12d55
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-11-28 22:16:44 -0700

    Time: 576 ms (57.99%) | Memory: 20.6 MB (71.05%) - LeetSync

--- tree/main/DailyLeetcoding/15-3sum/3sum.py
+++ tree/main/DailyLeetcoding/15-3sum/3sum.py
@@ -1,32 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res, quad = [], []
+        n = len(nums)
+        res = []
 
-        def kSum(k, start, target):
-            if k == 2:
-                l, r = start, len(nums) - 1
-                while l < r:
-                    if nums[l] + nums[r] < target:
+        for i in range(n - 2):
+            # Skip duplicates for i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l, r = i + 1, n - 1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+
+                    # Skip duplicates for l and r
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    elif nums[l] + nums[r] > target:
+                    while l < r and nums[r] == nums[r + 1]:
                         r -= 1
-                    else:
-                        res.append(quad + [nums[l], nums[r]])
-                        l += 1
-                        r -= 1
-                        while l < r and nums[l] == nums[l - 1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r + 1]:
-                            r -= 1
-                return
 
-            for i in range(start, len(nums) - k + 1):
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
-                quad.append(nums[i])
-                kSum(k - 1, i + 1, target - nums[i])
-                quad.pop()
-
-        kSum(3, 0, 0)
         return res
