6443dce8849d110ee7af7ae373430a427ed17caf
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-01 19:14:56 -0700

    Time: 4 ms (54.82%) | Memory: 18 MB (35.38%) - LeetSync

--- tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
+++ tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
@@ -5,17 +5,13 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # 243 -> 342
-        # 564 -> 465
-        # 708 -> 807
-
         dummy = ListNode(None)
         l3 = dummy
 
         carry = 0
 
-        while l1 and l2:
-            s = l1.val + l2.val + carry
+        while l1 or l2:
+            s = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
             if carry > 0:
                 carry = 0
             if s > 9:
@@ -25,25 +21,10 @@ class Solution:
             newNode = ListNode(s)
             dummy.next = newNode
             dummy = dummy.next
-            l1 = l1.next
-            l2 = l2.next
-        
-        if l1 or l2:
-            while l1 or l2:
-                s = (l1.val if l1 else l2.val) + carry
-                if carry > 0:
-                    carry = 0
-                if s > 9:
-                    s = s % 10
-                    carry += 1
-                
-                newNode = ListNode(s)
-                dummy.next = newNode
-                dummy = dummy.next
-                if l1:
-                    l1 = l1.next
-                else:
-                    l2 = l2.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
         
         if carry > 0:
             dummy.next = ListNode(1)
