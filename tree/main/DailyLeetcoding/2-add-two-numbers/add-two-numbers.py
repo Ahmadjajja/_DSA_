35588797d3bff2d72826717122f886195254e4f0
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-01 19:07:26 -0700

    Time: 4 ms (54.82%) | Memory: 17.8 MB (84.58%) - LeetSync

--- tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
+++ tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
@@ -1,21 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
+        # 243 -> 342
+        # 564 -> 465
+        # 708 -> 807
+
+        dummy = ListNode(None)
+        l3 = dummy
+
         carry = 0
+
+        while l1 and l2:
+            s = l1.val + l2.val + carry
+            if carry > 0:
+                carry = 0
+            if s > 9:
+                s = s % 10
+                carry += 1
+            
+            newNode = ListNode(s)
+            dummy.next = newNode
+            dummy = dummy.next
+            l1 = l1.next
+            l2 = l2.next
         
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-            
-            total = v1 + v2 + carry
-            carry = total // 10
-            cur.next = ListNode(total % 10)
-            cur = cur.next
-            
-            if l1:
+        if l1:
+            while l1:
+                s = l1.val + carry
+                if carry > 0:
+                    carry = 0
+                if s > 9:
+                    s = s % 10
+                    carry += 1
+                
+                newNode = ListNode(s)
+                dummy.next = newNode
+                dummy = dummy.next
                 l1 = l1.next
-            if l2:
+        if l2:
+            while l2:
+                s = l2.val + carry
+                if carry > 0:
+                    carry = 0
+                if s > 9:
+                    s = s % 10
+                    carry += 1
+                
+                newNode = ListNode(s)
+                dummy.next = newNode
+                dummy = dummy.next
                 l2 = l2.next
         
-        return dummy.next
+        if carry > 0:
+            dummy.next = ListNode(1)
+        
+        return l3.next
+
