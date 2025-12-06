614bb1c74f48c7631cb86b93731bcce57c282a3c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-05 23:06:19 -0700

    Time: 11 ms (91.48%) | Memory: 18.8 MB (29.29%) - LeetSync

--- tree/main/DailyLeetcoding/55-jump-game/jump-game.py
+++ tree/main/DailyLeetcoding/55-jump-game/jump-game.py
@@ -1,17 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        maxReach = 0  # farthest index we can reach so far
+        goal = len(nums) - 1
 
-        for i in range(len(nums)):
-            # if we are standing on an index we can't reach
-            if i > maxReach:
-                return False
-
-            # update farthest reachable index
-            maxReach = max(maxReach, i + nums[i])
-
-            # if we can reach or pass the last index
-            if maxReach >= len(nums) - 1:
-                return True
-
-        return True  # if loop finishes, last index is reachable
+        for i in range(len(nums) -1, -1, -1):
+            if (i + nums[i]) >= goal:
+                goal = i
+        
+        return goal == 0
