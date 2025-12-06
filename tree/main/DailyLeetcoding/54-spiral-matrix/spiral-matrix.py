73103e63ae7adc5507496f17421a7e63b0791c5f
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-05 23:36:20 -0700

    Time: 3 ms (2.46%) | Memory: 17.8 MB (39.13%) - LeetSync

--- tree/main/DailyLeetcoding/54-spiral-matrix/spiral-matrix.py
+++ tree/main/DailyLeetcoding/54-spiral-matrix/spiral-matrix.py
@@ -1,82 +1,23 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        ans = []
-        row, col = 0, 0
-        right, bottom, left, top = True, False, False, False
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
 
+        while left < right and top < bottom:
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+            if not (left < right and top < bottom):
+                break
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
 
-        while right or bottom or left or top:
-
-            # right
-            if right:
-                # keep increasing the col untill the wall
-                while col < len(matrix[0]) and matrix[row][col] != 101:
-                    ans.append(matrix[row][col])
-                    matrix[row][col] = 101
-                    col += 1
-                col -= 1
-
-                if len(matrix) != 1:
-                    row += 1
-
-                right = False
-                if matrix[row][col] != 101:
-                    bottom = True
-
-
-
-
-            # bottom
-            if bottom:
-                # keep increasing the row untill the wall
-                while row < len(matrix) and matrix[row][col] != 101:
-                    ans.append(matrix[row][col])
-                    matrix[row][col] = 101
-                    row += 1
-                
-                row -= 1
-                if len(matrix[0]) != 1:
-                    col -= 1
-                bottom = False
-                if matrix[row][col] != 101:
-                    left = True
-
-
-            # left
-            if left:
-                # keep decreasing the col untill the wall
-                while col > -1 and matrix[row][col] != 101:
-                    ans.append(matrix[row][col])
-                    matrix[row][col] = 101
-                    col -= 1
-                
-                col += 1
-                if len(matrix) != 1:
-                    row -= 1
-                left = False
-                if matrix[row][col] != 101:
-                    top = True
-
-
-            # top
-            if top:
-                # keep decreasing the row untill the wall
-                while row > -1 and matrix[row][col] != 101:
-                    ans.append(matrix[row][col])
-                    matrix[row][col] = 101
-                    row -= 1
-                
-                row += 1
-                if len(matrix[0]) != 1:
-                    col += 1
-                top = False
-                if matrix[row][col] != 101:
-                    right = True
-        
-        return ans
-        
-
-
-
-
-        
+        return res
