2fb28e00b132770a44a04cae14dc8e386347881c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-08 19:40:20 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.38%) - LeetSync

--- tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
+++ tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
@@ -3,31 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(None)
-        l3 = dummy
 
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
         carry = 0
 
-        while l1 or l2:
-            s = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
-            if carry > 0:
-                carry = 0
-            if s > 9:
-                s = s % 10
-                carry += 1
-            
-            newNode = ListNode(s)
-            dummy.next = newNode
-            dummy = dummy.next
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-        
-        if carry > 0:
-            dummy.next = ListNode(1)
-        
-        return l3.next
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
+            total = v1 + v2 + carry
+            carry = total // 10
+            val = total % 10
+
+            cur.next = ListNode(val)
+            cur = cur.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        return dummy.next
