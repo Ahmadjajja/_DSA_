3247c55f04f32a555093680a556828a55ae41b3e
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-12 11:51:43 -0700

    Time: 3 ms (23.16%) | Memory: 17.6 MB (88.44%) - LeetSync

--- tree/main/DailyLeetcoding/62-unique-paths/unique-paths.py
+++ tree/main/DailyLeetcoding/62-unique-paths/unique-paths.py
@@ -1,24 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        matrix = [[0] * n for _ in range(m)]
-        matrix[0][0] = 1
-
-        for row in range(m):
-            for col in range(n):
-                if row == 0 and col == 0:
-                    continue
-
-                currentVal = 0
-                # check if top is valid
-                if 0 <= row - 1 < m:
-                    currentVal += matrix[row - 1][col]
-
-                # check if left is valid
-                if 0 <= col - 1 < n:
-                    currentVal += matrix[row][col - 1]
-                
-                matrix[row][col] = currentVal
-        print("matrix : ", matrix)
-        return matrix[m - 1][n - 1]
-        
-        
+        dp = [1] * n
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        return dp[-1]
