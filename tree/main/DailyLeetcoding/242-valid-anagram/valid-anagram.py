dc00686bd78d9b8debef83005044339964400ee2
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-14 13:40:17 -0700

    Time: 15 ms (41.17%) | Memory: 17.9 MB (62.72%) - LeetSync

--- tree/main/DailyLeetcoding/242-valid-anagram/valid-anagram.py
+++ tree/main/DailyLeetcoding/242-valid-anagram/valid-anagram.py
@@ -1,4 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        freqS = {}
+        freqT = {}
+
+        for ch in s:
+            freqS[ch] = freqS.get(ch, 0) + 1
+        for ch in t:
+            freqT[ch] = freqT.get(ch, 0) + 1
+        print(freqS)
+        print(freqT)
+        print(Counter(s))
+        print(Counter(t))
+
+        return freqS == freqT
         
