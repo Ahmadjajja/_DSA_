88ce97162e1596ef10bd76c732ef10038d71f6cd
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-12-22 20:16:29 -0700

    Time: 51 ms (33.11%) | Memory: 23 MB (95.04%) - LeetSync

---
+++ tree/main/DailyLeetcoding/917-boats-to-save-people/boats-to-save-people.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort() # nlogn
+
+        left = 0
+        right = len(people) - 1
+        countBoats = 0
+
+        while left <= right:
+
+            sum = people[left] + people[right]
+            if sum <= limit:
+                countBoats += 1
+                left += 1
+                right -= 1
+            else:
+                countBoats += 1
+                right -= 1
+        
+        return countBoats
+        
+
+
+        
