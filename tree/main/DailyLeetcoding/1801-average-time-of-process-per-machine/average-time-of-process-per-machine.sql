c6b88a2b8e081e016aad1e66eb2ca72bccccdc89
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2026-03-25 22:28:26 -0600

    Time: 241 ms (53.97%) | Memory: 0B (100.00%) - LeetSync

--- tree/main/DailyLeetcoding/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ tree/main/DailyLeetcoding/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,8 +2,23 @@
 
 
 
-select a1.machine_id, round(avg(a1.timestamp - a2.timestamp), 3) as processing_time  from Activity as a1
-join Activity as a2
-on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
-where a1.activity_type = 'end' and a2.activity_type = "start"
-group by a1.machine_id
+-- select a1.machine_id, round(avg(a1.timestamp - a2.timestamp), 3) as processing_time  from Activity as a1
+-- join Activity as a2
+-- on a1.machine_id = a2.machine_id and a1.process_id = a2.process_id
+-- where a1.activity_type = 'end' and a2.activity_type = "start"
+-- group by a1.machine_id
+
+SELECT 
+    a1.machine_id,
+    ROUND(AVG(
+        a1.timestamp - (
+            SELECT a2.timestamp
+            FROM Activity AS a2
+            WHERE a2.machine_id = a1.machine_id
+              AND a2.process_id = a1.process_id
+              AND a2.activity_type = 'start'
+        )
+    ), 3) AS processing_time
+FROM Activity AS a1
+WHERE a1.activity_type = 'end'
+GROUP BY a1.machine_id;
