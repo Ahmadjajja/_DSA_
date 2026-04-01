8989be06744006dbd70dc1f53b82ee2733bab22e
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-03-31 21:05:02 -0600

    Time: 2217 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- tree/main/DailyLeetcoding/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ tree/main/DailyLeetcoding/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,9 +1,20 @@
+-- SELECT
+--     contest_id,
+--     ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+-- FROM Register AS r
+-- GROUP BY
+--     r.contest_id
+-- ORDER BY
+--     percentage DESC,
+--     contest_id ASC;
+
 SELECT
-    contest_id,
-    ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+    r.contest_id,
+    ROUND(
+        COUNT(DISTINCT r.user_id) * 100.0 / COUNT(DISTINCT u.user_id),
+        2
+    ) AS percentage
 FROM Register AS r
-GROUP BY
-    r.contest_id
-ORDER BY
-    percentage DESC,
-    contest_id ASC;
+CROSS JOIN Users AS u
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
