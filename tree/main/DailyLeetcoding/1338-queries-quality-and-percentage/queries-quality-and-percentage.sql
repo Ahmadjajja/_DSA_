69e459bdf0d87da21920ccbeb89ab8a5aa464e73
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-04-01 20:59:13 -0600

    Time: 334 ms (80.35%) | Memory: 0B (100.00%) - LeetSync

--- tree/main/DailyLeetcoding/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ tree/main/DailyLeetcoding/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,12 +1,12 @@
-select query_name, 
-       round(avg(rating / position), 2) as quality,
-       round(((
-        select count(*) from Queries
-        where rating < 3 and query_name = q.query_name
-       ) / count(*)) * 100, 2) as poor_query_percentage
+-- select query_name, 
+--        round(avg(rating / position), 2) as quality,
+--        round(((
+--         select count(*) from Queries
+--         where rating < 3 and query_name = q.query_name
+--        ) / count(*)) * 100, 2) as poor_query_percentage
 
-from Queries as q
-group by query_name
+-- from Queries as q
+-- group by query_name
 
 -- SELECT 
 --     query_name,
@@ -14,3 +14,10 @@ group by query_name
 --     ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2) AS poor_query_percentage
 -- FROM Queries
 -- GROUP BY query_name;
+
+SELECT 
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
