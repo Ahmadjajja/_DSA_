c9ac52ff6d48cfe514a43e7ba636cccd3d867e48
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-04-23 21:29:28 -0600

    Time: 501 ms (87.02%) | Memory: 0B (100.00%) - LeetSync

--- tree/main/DailyLeetcoding/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ tree/main/DailyLeetcoding/1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,7 +1,16 @@
-# Write your MySQL query statement below
-select employee_id, department_id from Employee
-where primary_flag = 'Y'
-union
-select employee_id, department_id from Employee
-group by employee_id
-having count(*) = 1
+-- # Write your MySQL query statement below
+-- select employee_id, department_id from Employee
+-- where primary_flag = 'Y'
+-- union
+-- select employee_id, department_id from Employee
+-- group by employee_id
+-- having count(*) = 1
+
+-- left join
+
+select distinct e1.employee_id, e1.department_id
+from Employee e1
+left join Employee e2 
+  on e1.employee_id = e2.employee_id 
+  and e1.department_id != e2.department_id
+where e1.primary_flag = 'Y' or e2.employee_id is null
