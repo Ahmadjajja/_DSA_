4c6f94cec82065d5438dc7e46c910b0fb2579853
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-06-08 21:38:07 -0600

    Time: 263 ms (25.75%) | Memory: 31.4 MB (5.39%) - LeetSync

--- tree/main/DailyLeetcoding/200-number-of-islands/number-of-islands.py
+++ tree/main/DailyLeetcoding/200-number-of-islands/number-of-islands.py
@@ -1,31 +1,47 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
+
+
+        visited = set() # this is gonna contain tuple (i, j)
+        islands = 0
+        dirArr = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         rows, cols = len(grid), len(grid[0])
-        visited = set()
-        dirArr = [(0,1),(0,-1),(1,0),(-1,0)]
+        # if rows == 1 and cols == 1:
+        #     return grid[0][0] == "1"
 
         def dfs(r, c):
-            if (
-                r < 0 or r >= rows or
-                c < 0 or c >= cols or
-                grid[r][c] == "0" or
-                (r, c) in visited
-            ):
-                return
-            
+
             visited.add((r, c))
 
             for dr, dc in dirArr:
-                dfs(r + dr, c + dc)
+                nr = r + dr
+                nc = c + dc
+                # checking out of range
+                if nr >= rows or nr < 0 or nc >= cols or nc < 0 or grid[nr][nc] == "0" or (nr, nc) in visited:
+                    continue
+                
+                dfs(nr, nc)
 
 
-        islands = 0
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    dfs(r, c)
+        for row in range(rows):
+            for col in range(cols):
+
+                cur = grid[row][col]
+                if cur == "0" or (row, col) in visited:
+                    continue
+                else:
+
+                    dfs(row, col)
                     islands += 1
-
+        
         return islands
+
+# tc => O()
+# sc => O()
+
+
+
+
+        
