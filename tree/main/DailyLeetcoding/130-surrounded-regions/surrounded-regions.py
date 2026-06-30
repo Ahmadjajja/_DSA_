5d7a0bad9e551f9e4f873b7aaaf817a7c45f337d
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-06-29 21:29:23 -0600

    Time: 0 ms (100.00%) | Memory: 22.4 MB (58.87%) - LeetSync

--- tree/main/DailyLeetcoding/130-surrounded-regions/surrounded-regions.py
+++ tree/main/DailyLeetcoding/130-surrounded-regions/surrounded-regions.py
@@ -8,15 +8,16 @@ class Solution:
             ):
                 return
             board[r][c] = "T"
-            
-            dirArr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            for dr, dc in dirArr:
-                capture(r + dr, c + dc)
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
 
         for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O" and ( r in [0, ROWS - 1] or c in [0, COLS - 1] ):
-                    capture(r, c)
+            if board[r][0] == "O":
+                capture(r, 0)
+            if board[r][COLS - 1] == "O":
+                capture(r, COLS - 1)
 
         for c in range(COLS):
             if board[0][c] == "O":
@@ -30,7 +31,3 @@ class Solution:
                     board[r][c] = "X"
                 elif board[r][c] == "T":
                     board[r][c] = "O"
-
-
-# 1. adjcensy list
-# 2. directions array [(0, 1), (1, 0), (0, -1), (-1, 0)]
