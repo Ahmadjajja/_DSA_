1939cabf9ab3c8fb48b1950efb924efe948514b9
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-08-28 12:11:06 +0500

    Time: 0 ms (100.00%) | Memory: 21 MB (7.09%) - LeetSync

--- tree/main/DailyLeetcoding/1-two-sum/two-sum.py
+++ tree/main/DailyLeetcoding/1-two-sum/two-sum.py
@@ -1,14 +1,36 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # what would be len of nums? 
+        # [2, 7, 11, 15]
+        # is it sorted already?
+
+        # 1. brute force -> O(n2)
+        # 2. little better solution -> O(nlogn)
+        # 3. tc -> O(n), sc -> O(n)
 
         hm = {}
+        {3: 0, 4: 1, }
 
-        for index in range(len(nums)):
-            if nums[index] in hm:
-                return [index, hm[nums[index]]]
+        for i in range(len(nums)):
+            n = nums[i]
+            if n in hm: # constant lookup
+                return [hm[n], i]
+            
+            secondElem = target - n
+            hm[secondElem] = i
 
-            hm[target - nums[index]] = index
-        
-        return [-1, -1]
+        # {
+        #    7 : 0,
+        #    
+        # 
+        #  }
+
+
+        # -ve or +ve nums?
+        # how big 1 number might be?
+        # -ve or +ve target?
+        # how big 1 target might be?
+
+
 
         
