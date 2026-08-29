606debea8d9575d83e858ff26308776de369b7ae
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-08-29 18:48:17 +0500

    Time: 124 ms (17.08%) | Memory: 36.5 MB (5.35%) - LeetSync

--- tree/main/DailyLeetcoding/739-daily-temperatures/daily-temperatures.py
+++ tree/main/DailyLeetcoding/739-daily-temperatures/daily-temperatures.py
@@ -1,13 +1,18 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        monotonic_stack = []  # decreasing order, stores [temp, index]
+        monotonic_stack = [] # decreasing order
         output = [0] * len(temperatures)
 
         for i, t in enumerate(temperatures):
+
             while monotonic_stack and t > monotonic_stack[-1][0]:
+
                 temp = monotonic_stack.pop()
+
                 output[temp[1]] = i - temp[1]
 
             monotonic_stack.append([t, i])
-
+        
         return output
+
+        
