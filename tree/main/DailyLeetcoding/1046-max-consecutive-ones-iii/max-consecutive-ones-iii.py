6ec416b9c1249aa9a580c77bccec8e91cd26a4c1
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-04 08:30:30 +0500

    Time: 50 ms (86.59%) | Memory: 22.3 MB (6.44%) - LeetSync

--- tree/main/DailyLeetcoding/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ tree/main/DailyLeetcoding/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,30 +1,30 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        l, r = 0, 0
+        longestWindow = 0
+        freqOnes = 0
 
-        longest1s = 0
-        countZeros = 0
+        l = 0
 
-        while r < len(nums):
-            if countZeros > k:
-                if nums[l] == 0:
-                    countZeros -= 1
-                l += 1
-                continue
-
-            longest1s = max(longest1s, r - l)
+        for r in range(len(nums)):
             if nums[r] == 0:
-                countZeros += 1
-            r += 1
+                freqOnes += 1
+            if freqOnes <= k:
+                windowSize = r - l + 1
+                longestWindow = max(longestWindow, windowSize)
+                continue
+            
+            while freqOnes > k:
+                if nums[l] == 0:
+                    freqOnes -= 1
+                l += 1
         
-        if countZeros <= k:
-            longest1s = max(longest1s, r - l)
-
-        return longest1s 
+        return longestWindow
 
 
-# tc = O(n)
-# sc = O(1)
+
+
+
+
 
 
         
