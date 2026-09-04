4144507d8cfc7a87c2f9494f678ef3fb1f2017e0
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-04 08:34:20 +0500

    Time: 61 ms (44.25%) | Memory: 22.2 MB (51.15%) - LeetSync

--- tree/main/DailyLeetcoding/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ tree/main/DailyLeetcoding/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -8,17 +8,20 @@ class Solution:
         for r in range(len(nums)):
             if nums[r] == 0:
                 freqOnes += 1
-            if freqOnes <= k:
-                windowSize = r - l + 1
-                longestWindow = max(longestWindow, windowSize)
-                continue
-            
             while freqOnes > k:
                 if nums[l] == 0:
                     freqOnes -= 1
                 l += 1
+                
+            windowSize = r - l + 1
+            longestWindow = max(longestWindow, windowSize)
+            
         
         return longestWindow
+
+        # tc -> O(n)
+        # sc -> O(1)
+
 
 
 
