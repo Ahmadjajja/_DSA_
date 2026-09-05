62a56bf4b8448827f143b5cf3ace549f3c4b29c6
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-05 14:43:08 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.68%) - LeetSync

--- tree/main/DailyLeetcoding/198-house-robber/house-robber.py
+++ tree/main/DailyLeetcoding/198-house-robber/house-robber.py
@@ -1,17 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        cache = {}
+        # cache = {}
 
-        def dfs(index):
-            if index >= len(nums):
-                return 0
-            if index in cache:
-                return cache[index]
+        # def dfs(index):
+        #     if index >= len(nums):
+        #         return 0
+        #     if index in cache:
+        #         return cache[index]
 
-            skip = dfs(index + 1)
-            take = nums[index] + dfs(index + 2)
+        #     skip = dfs(index + 1)
+        #     take = nums[index] + dfs(index + 2)
 
-            cache[index] = max(skip, take)
-            return cache[index]
+        #     cache[index] = max(skip, take)
+        #     return cache[index]
 
-        return dfs(0)
+        # return dfs(0)
+
+        rob1, rob2 = 0, 0
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
