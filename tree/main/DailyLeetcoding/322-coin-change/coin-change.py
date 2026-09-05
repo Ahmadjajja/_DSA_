df11a376d7c6d85f114717555ce4d79421c9d1f2
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-05 15:22:14 -0600

    Time: 913 ms (19.57%) | Memory: 71.8 MB (9.65%) - LeetSync

--- tree/main/DailyLeetcoding/322-coin-change/coin-change.py
+++ tree/main/DailyLeetcoding/322-coin-change/coin-change.py
@@ -1,23 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
+        cache = {}
 
-        def dfs(amount):
-            if amount == 0:
-                return 0
-            if amount < 0:
+        def dfs(sum):
+            if sum > amount:
                 return float("inf")
-            
-            if amount in memo:
-                return memo[amount]
-            
-            minCoins = float("inf")
-            for coin in coins:
-                minCoins = min(minCoins, dfs(amount - coin) + 1)
-            
-            memo[amount] = minCoins
-            return memo[amount] 
+            if sum == amount:
+                return 0
+            if sum in cache:
+                return cache[sum]
 
-        ans = dfs(amount)
+            minCoins = float('inf')
+            for coin in coins:
+                minCoins = min(minCoins, dfs(sum + coin))
+            
+            cache[sum] = 1 + minCoins 
+
+            return cache[sum]
+        ans = dfs(0)
         return ans if ans != float('inf') else -1
-        
