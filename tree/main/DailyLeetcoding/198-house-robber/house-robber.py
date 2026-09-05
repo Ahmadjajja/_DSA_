e39bd382dc0540400e1ce755c4fd960c8e9d3228
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-05 13:40:58 -0600

    Time: 1022 ms (0.64%) | Memory: 265.4 MB (6.29%) - LeetSync

--- tree/main/DailyLeetcoding/198-house-robber/house-robber.py
+++ tree/main/DailyLeetcoding/198-house-robber/house-robber.py
@@ -1,31 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # # top-down
-        # memo = {}
+        cache = {}
+
+        def dfs(index, sum):
+            if index >= len(nums):
+                return sum
+            
+            if (index, sum) in cache:
+                return cache[(index, sum)]
+            
+            # skip
+            left = dfs(index + 1, sum)
+            # choose
+            right = dfs(index + 2, sum + nums[index])
+
+            cache[(index, sum)] = max(left, right)
+
+            return cache[(index, sum)]
         
-        # def dfs(i):
-        #     if i >= len(nums):
-        #         return 0
-            
-        #     if i in memo:
-        #         return memo[i]
-            
-        #     rob_this = nums[i] + dfs(i + 2)
-        #     skip_this = dfs(i + 1)
-            
-        #     memo[i] = max(rob_this, skip_this)
-        #     return memo[i]
+        return dfs(0, 0)
         
-        # return dfs(0)
-        # bottom-up approach
-
-        if len(nums) == 1:
-            return nums[0]
-
-        first, second = 0, 0
-        for num in nums:
-            cur = max(first + num, second)
-            first = second
-            second = cur
-        return second
-
