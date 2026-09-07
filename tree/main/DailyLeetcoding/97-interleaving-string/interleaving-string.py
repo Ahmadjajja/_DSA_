45f6fa240a14ce781847627e699722ed904f20f3
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-07 17:53:03 -0600

    Time: 43 ms (91.98%) | Memory: 20.2 MB (22.62%) - LeetSync

--- tree/main/DailyLeetcoding/97-interleaving-string/interleaving-string.py
+++ tree/main/DailyLeetcoding/97-interleaving-string/interleaving-string.py
@@ -1,31 +1,23 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) > len(s3) or len(s1) + len(s2) < len(s3):
+        if len(s1) + len(s2) != len(s3):
             return False
 
-        memo = {}
+        cache = {}
 
-        def dfs(i1, i2, i3, st):
-            if i1 >= len(s1) and i2 >= len(s2):
-                return st == s3
-            
-            if i1 < len(s1) and s3[i3] != s1[i1] and i2 < len(s2) and s3[i3] != s2[i2]:
-                return False
-            
-            if (i1, i2, i3) in memo:
-                return memo[(i1, i2, i3)]
+        def dfs(index1, index2):
+            if index1 == len(s1) and index2 == len(s2):
+                return True
+            if (index1, index2) in cache:
+                return cache[(index1, index2)]
 
-            left = False
-            if i1 < len(s1) and s3[i3] == s1[i1]:
-                left = dfs(i1 + 1, i2, i3 + 1, st + s3[i3])
-            right = False
-            if i2 < len(s2) and s3[i3] == s2[i2]:
-                right = dfs(i1, i2 + 1, i3 + 1, st + s3[i3])
-            
-            memo[(i1, i2, i3)] = left or right
-            
-            return left or right
+            ans = False
+            if index1 < len(s1) and s1[index1] == s3[index1 + index2]:
+                ans = ans or dfs(index1 + 1, index2)
+            if index2 < len(s2) and s2[index2] == s3[index1 + index2]:
+                ans = ans or dfs(index1, index2 + 1)
 
-        return dfs(0, 0, 0, "")
+            cache[(index1, index2)] = ans
+            return ans
 
-        
+        return dfs(0, 0)
