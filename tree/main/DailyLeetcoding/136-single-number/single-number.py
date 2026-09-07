03604ca30bb07d41f3c6301db2c3336463d072bf
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-07 12:02:03 -0600

    Time: 4 ms (41.54%) | Memory: 21 MB (93.68%) - LeetSync

--- tree/main/DailyLeetcoding/136-single-number/single-number.py
+++ tree/main/DailyLeetcoding/136-single-number/single-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-
-        for n in nums:
-            ans ^= n
-        
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            ans = ans ^ nums[i]
         return ans
         
