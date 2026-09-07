b77ee5a5a38d8aca63954b6bb1ac594c22039033
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-06 22:22:40 -0600

    Time: 14 ms (90.27%) | Memory: 20.6 MB (18.95%) - LeetSync

--- tree/main/DailyLeetcoding/402-remove-k-digits/remove-k-digits.py
+++ tree/main/DailyLeetcoding/402-remove-k-digits/remove-k-digits.py
@@ -6,9 +6,7 @@ class Solution:
                 k -= 1
                 stack.pop()
             stack.append(ch)
-
         if k > 0:
             stack = stack[:len(stack) - k]
-
         res = "".join(stack).lstrip("0")
-        return res if res != "" else "0"
+        return  res if res != "" else "0"
