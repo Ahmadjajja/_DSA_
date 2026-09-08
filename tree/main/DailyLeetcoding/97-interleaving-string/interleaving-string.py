57b8b7559fd22ddd09f678cbc13f0ea7f531df33
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-07 18:13:54 -0600

    Time: 49 ms (69.00%) | Memory: 19.3 MB (72.28%) - LeetSync

--- tree/main/DailyLeetcoding/97-interleaving-string/interleaving-string.py
+++ tree/main/DailyLeetcoding/97-interleaving-string/interleaving-string.py
@@ -3,21 +3,34 @@ class Solution:
         if len(s1) + len(s2) != len(s3):
             return False
 
-        cache = {}
+        dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
 
-        def dfs(index1, index2):
-            if index1 == len(s1) and index2 == len(s2):
-                return True
-            if (index1, index2) in cache:
-                return cache[(index1, index2)]
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True 
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+        return dp[0][0]
 
-            ans = False
-            if index1 < len(s1) and s1[index1] == s3[index1 + index2]:
-                ans = ans or dfs(index1 + 1, index2)
-            if index2 < len(s2) and s2[index2] == s3[index1 + index2]:
-                ans = ans or dfs(index1, index2 + 1)
+        # recursion + cache 
 
-            cache[(index1, index2)] = ans
-            return ans
+        # if len(s1) + len(s2) != len(s3):
+        #     return False
 
-        return dfs(0, 0)
+        # def dfs(index1, index2):
+        #     if index1 == len(s1) and index2 == len(s2):
+        #         return True
+        #     if (index1, index2) in cache:
+        #         return cache[(index1, index2)]
+
+        #     if index1 < len(s1) and s1[index1] == s3[index1 + index2] and dfs(index1 + 1, index2):
+        #         return True 
+        #     if index2 < len(s2) and s2[index2] == s3[index1 + index2] and dfs(index1, index2 + 1):
+        #         return True
+
+        #     cache[(index1, index2)] = False
+        #     return False
+
+        # return dfs(0, 0)
