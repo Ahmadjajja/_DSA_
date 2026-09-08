f5d2510634683eac099d5b887292c003ba0a25a3
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-08 12:32:33 -0600

    Time: 378 ms (50.95%) | Memory: 19.5 MB (83.86%) - LeetSync

--- tree/main/DailyLeetcoding/518-coin-change-ii/coin-change-ii.py
+++ tree/main/DailyLeetcoding/518-coin-change-ii/coin-change-ii.py
@@ -1,26 +1,45 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        cache = {}
+        # bottom up
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for c in range(len(coins) - 1, -1, -1):
+            next_dp = [0] * (amount + 1)
+            next_dp[0] = 1
 
-        def dfs(index, curAmount):
-            if curAmount == amount:
-                return 1
-            if curAmount > amount or index >= len(coins):
-                return 0
-
-            if (index, curAmount) in cache:
-                return cache[(index, curAmount)]
-
-            # add curAmount
-            left = dfs(index, curAmount + coins[index])
-            # skip curAmount
-            right = dfs(index + 1, curAmount)
-
-            cache[(index, curAmount)] = left + right
+            for a in range(1, amount + 1):
+                next_dp[a] = dp[a] 
+                if a - coins[c] >= 0:
+                    next_dp[a] += next_dp[a - coins[c]]
+            dp = next_dp
+        return next_dp[amount]
             
-            return cache[(index, curAmount)]
 
-        return dfs(0, 0)
+        # top down 
+        # cache = {}
+
+        # def dfs(index, curAmount):
+        #     if curAmount == amount:
+        #         return 1
+        #     if curAmount > amount or index >= len(coins):
+        #         return 0
+
+        #     if (index, curAmount) in cache:
+        #         return cache[(index, curAmount)]
+
+        #     # add curAmount
+        #     left = dfs(index, curAmount + coins[index])
+        #     # skip curAmount
+        #     right = dfs(index + 1, curAmount)
+
+        #     cache[(index, curAmount)] = left + right
+
+        #     return cache[(index, curAmount)]
+
+        # return dfs(0, 0)
+
+        # # tc -> O(n * m)
+        # # sc -> O(n * m)
 
 
         
