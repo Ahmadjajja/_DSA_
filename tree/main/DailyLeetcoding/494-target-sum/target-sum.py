11bb7e90711cf066604ec05a2076b779eda13759
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-08 09:10:37 -0600

    Time: 119 ms (57.70%) | Memory: 19.2 MB (90.80%) - LeetSync

--- tree/main/DailyLeetcoding/494-target-sum/target-sum.py
+++ tree/main/DailyLeetcoding/494-target-sum/target-sum.py
@@ -1,22 +1,42 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        cache = {}
-        def dfs(i, res):
-            if i == len(nums):
-                if res == target:
-                    return 1
-                else:
-                    return 0
-            
-            if (i, res) in cache:
-                return cache[(i, res)]
-            
-            left = dfs(i + 1, res - nums[i])
-            right = dfs(i + 1, res + nums[i])
+        
+        dp = defaultdict(int)
+        dp[0] = 1
 
-            cache[(i, res)] = left + right
-            
-            return cache[(i, res)]
+        for i in range(len(nums)):
+            next_dp = defaultdict(int)
+            for cur_sum, count in dp.items():
+                next_dp[cur_sum + nums[i]] += count
+                next_dp[cur_sum - nums[i]] += count
+            dp = next_dp
 
-        return dfs(0, 0)
+        return dp[target]
+
+        # tc -> O(n)
+        # sc -> O(n)
+
+
+        # cache = {}
+        # def dfs(i, res):
+        #     if i == len(nums):
+        #         if res == target:
+        #             return 1
+        #         else:
+        #             return 0
+            
+        #     if (i, res) in cache:
+        #         return cache[(i, res)]
+            
+        #     left = dfs(i + 1, res - nums[i])
+        #     right = dfs(i + 1, res + nums[i])
+
+        #     cache[(i, res)] = left + right
+            
+        #     return cache[(i, res)]
+
+        # return dfs(0, 0)
+
+        # # tc -> O(n)
+        # # sc -> O(n)
         
