499ba7cde1918e9d0c1e3a8cd6d7300834607daa
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-07 22:16:51 -0600

    Time: 62 ms (60.23%) | Memory: 23.9 MB (71.29%) - LeetSync

--- tree/main/DailyLeetcoding/937-online-stock-span/online-stock-span.py
+++ tree/main/DailyLeetcoding/937-online-stock-span/online-stock-span.py
@@ -1,5 +1,30 @@
+# class StockSpanner:
+#     def __init__(self):
+#         self.stockPrices = []
+#         self.output = []
+
+#     def next(self, price: int) -> int:
+#         curSpan = 1
+#         i = len(self.stockPrices) - 1
+#         while i >= 0 and self.stockPrices[i] <= price:
+#             curSpan += self.output[i]
+#             i = i - self.output[i]
+#         self.stockPrices.append(price)
+#         self.output.append(curSpan)
+#         return curSpan
+
 class StockSpanner:
 
+    def __init__(self):
+        self.stack = []  # pair: (price, span)
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
+        return span
     # [7, 3, 2, 1, 2], 5
     # [1, 1, 1, 1, 3], 3 + 1 = 4
 
@@ -8,24 +33,6 @@ class StockSpanner:
     # 5 - 2 = 3 values (3, 4, 5)
 
     # [7, 34, 1, 2], 8
-
-    def __init__(self):
-        self.stockPrices = []
-        self.output = []
-
-        
-
-    def next(self, price: int) -> int:
-        curSpan = 1
-        i = len(self.stockPrices) - 1
-        while i >= 0 and self.stockPrices[i] <= price:
-            curSpan += self.output[i]
-            i = i - self.output[i]
-        self.stockPrices.append(price)
-        self.output.append(curSpan)
-        return curSpan
-
-
 
 
 
