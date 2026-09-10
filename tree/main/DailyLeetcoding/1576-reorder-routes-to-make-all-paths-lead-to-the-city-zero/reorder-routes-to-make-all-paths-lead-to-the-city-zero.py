7fdc1eaed339ef12f1162efdc6bad052c6d85f8b
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-09 22:12:58 -0600

    Time: 173 ms (28.48%) | Memory: 99.2 MB (5.11%) - LeetSync

--- tree/main/DailyLeetcoding/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ tree/main/DailyLeetcoding/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,24 +1,20 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        outward = {i: [] for i in range(n)}
-        inward = {i: [] for i in range(n)}
+        graph = {i: [] for i in range(n)}
 
         for src, dst in connections:
-            outward[src].append(dst)
-            inward[dst].append(src)
+            graph[src].append((dst, 1))
+            graph[dst].append((src, 0))
+            
         count = 0
         visit = set()
         def dfs(node):
             nonlocal count
             visit.add(node)
-            for nei in outward[node]:
+            for nei, dirCost in graph[node]:
                 if nei in visit:
                     continue
-                count += 1
-                dfs(nei)
-            for nei in inward[node]:
-                if nei in visit:
-                    continue	
+                count += dirCost
                 dfs(nei)		
             
         dfs(0)
