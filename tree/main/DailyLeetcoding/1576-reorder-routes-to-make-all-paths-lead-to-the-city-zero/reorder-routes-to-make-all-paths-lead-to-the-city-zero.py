224b5aa2c4efd3e2d95aa7fbdbbcf28bfc636209
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-09 22:19:03 -0600

    Time: 164 ms (34.32%) | Memory: 99.6 MB (5.11%) - LeetSync

--- tree/main/DailyLeetcoding/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ tree/main/DailyLeetcoding/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -5,19 +5,17 @@ class Solution:
         for src, dst in connections:
             graph[src].append((dst, 1))
             graph[dst].append((src, 0))
-            
-        count = 0
+
         visit = set()
-        def dfs(node):
-            nonlocal count
+        def dfs(node, count):
             visit.add(node)
+            count = 0
             for nei, dirCost in graph[node]:
-                if nei in visit:
-                    continue
-                count += dirCost
-                dfs(nei)		
-            
-        dfs(0)
-        return count
+                if nei not in visit:
+                    count += dfs(nei, count) + dirCost
+            	
+            return count
+
+        return dfs(0, 0)
 
         
