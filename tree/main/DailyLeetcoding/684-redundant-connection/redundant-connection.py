e956661532a1d72b2e3e590985c75db40aa5d76f
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-09 23:52:55 -0600

    Time: 0 ms (100.00%) | Memory: 19.7 MB (50.80%) - LeetSync

--- tree/main/DailyLeetcoding/684-redundant-connection/redundant-connection.py
+++ tree/main/DailyLeetcoding/684-redundant-connection/redundant-connection.py
@@ -5,21 +5,14 @@ class Solution:
 
         def find(n):
             if par[n] != n:
-                return find(par[n])
-            else:
-                return n
+                par[n] = find(par[n])
+            return par[n]
 
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
 
             if p1 == p2:
                 return False
-            # if rank[p1] > rank[p2]:
-            #     par[p2] = p1
-            #     rank[p1] += rank[p2]
-            # else:
-            #     par[p1] = p2
-            #     rank[p2] += rank[p1]
             par[p2] = p1
             return True
 
