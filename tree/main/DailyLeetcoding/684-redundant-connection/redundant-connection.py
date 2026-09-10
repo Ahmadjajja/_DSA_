7aae75db255a50f71a6a25ab3eed44f4f5c8c2ec
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-09 23:48:24 -0600

    Time: 18 ms (12.37%) | Memory: 19.8 MB (17.84%) - LeetSync

--- tree/main/DailyLeetcoding/684-redundant-connection/redundant-connection.py
+++ tree/main/DailyLeetcoding/684-redundant-connection/redundant-connection.py
@@ -4,11 +4,10 @@ class Solution:
         # rank = [1] * (len(edges) + 1)
 
         def find(n):
-            p = par[n]
-            while p != par[p]:
-                par[p] = par[par[p]]
-                p = par[p]
-            return p
+            if par[n] != n:
+                return find(par[n])
+            else:
+                return n
 
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
