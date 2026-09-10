2d6f401076dc8006b99faf6a18c8b064cb116ccb
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-10 13:00:41 -0600

    Time: 1931 ms (11.03%) | Memory: 20.4 MB (10.58%) - LeetSync

--- tree/main/DailyLeetcoding/300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ tree/main/DailyLeetcoding/300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,10 +1,34 @@
+# top down
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i + 1, len(nums)):
+        n = len(nums)
+        memo = [-1] * n
+
+        def dfs(i):
+            if memo[i] != -1:
+                return memo[i]
+
+            LIS = 1
+            for j in range(i + 1, n):
                 if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], 1 + LIS[j])
-        
-        return max(LIS)
+                    LIS = max(LIS, 1 + dfs(j))
+
+            memo[i] = LIS
+            return LIS
+
+        return max(dfs(i) for i in range(n))
+
+# # bottom up
+
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         LIS = [1] * len(nums)
+
+#         for i in range(len(nums) - 1, -1, -1):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] < nums[j]:
+#                     LIS[i] = max(LIS[i], 1 + LIS[j])
+#         return max(LIS)
+
         
