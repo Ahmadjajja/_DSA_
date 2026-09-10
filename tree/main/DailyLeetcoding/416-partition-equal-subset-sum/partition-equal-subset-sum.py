949c6eb4714f8b870d4b10e2810d31bf22229070
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-10 10:59:27 -0600

    Time: 297 ms (88.58%) | Memory: 20.8 MB (51.00%) - LeetSync

--- tree/main/DailyLeetcoding/416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ tree/main/DailyLeetcoding/416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,23 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        if total % 2 != 0:
+        if sum(nums) % 2:
             return False
-        target = total // 2
 
-        cache = {}
-        def dfs(curIndex, remaining):
-            if remaining == 0:
-                return True
-            if remaining < 0 or curIndex == len(nums):
-                return False
-            if (curIndex, remaining) in cache:
-                return cache[(curIndex, remaining)]
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
 
-            # either skip nums[curIndex] or include it
-            result = dfs(curIndex + 1, remaining) or dfs(curIndex + 1, remaining - nums[curIndex])
-
-            cache[(curIndex, remaining)] = result
-            return result
-
-        return dfs(0, target)
+        for i in range(len(nums) - 1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                if (t + nums[i]) == target:
+                    return True
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return False
