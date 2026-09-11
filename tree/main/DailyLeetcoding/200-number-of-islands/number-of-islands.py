a03d031204827d6d502f5126feba2922349c7c47
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-10 23:19:42 -0600

    Time: 231 ms (86.86%) | Memory: 31.1 MB (9.06%) - LeetSync

--- tree/main/DailyLeetcoding/200-number-of-islands/number-of-islands.py
+++ tree/main/DailyLeetcoding/200-number-of-islands/number-of-islands.py
@@ -1,34 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
-        dirArr = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        islands = 0
         rows, cols = len(grid), len(grid[0])
+        visit = set()
+        dirArr = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-        def dfs(r, c):
-            # base case
-            for row, col in dirArr:
-                nr, nc = r + row, c + col
-
-                if nr < 0 or nr >= rows or nc < 0 or nc >= cols or (nr, nc) in visited or grid[nr][nc] == '0':
+        def isIsland(row, col):
+            visit.add((row, col))
+            for r, c in dirArr:
+                nr, nc = row + r, col + c
+                if nr < 0 or nr >= rows or nc < 0 or nc >= cols or (nr, nc) in visit or grid[nr][nc] == '0':
                     continue
-                
-                visited.add((nr, nc))
-                dfs(nr, nc)
+                isIsland(nr, nc)
 
-        for row in range(rows):
-            for col in range(cols):
-                if (row, col) in visited or grid[row][col] == '0':
+        no_of_islands = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r, c) in visit or grid[r][c] == '0':
                     continue
-                visited.add((row, col))
-                dfs(row, col)
-                islands += 1
+                isIsland(r, c)
+                no_of_islands += 1
         
-        return islands
+        return no_of_islands 
 
 
 
 
-        
-
-        
