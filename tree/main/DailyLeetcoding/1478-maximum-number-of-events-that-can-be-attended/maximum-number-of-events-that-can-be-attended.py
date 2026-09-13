03924db8101a12242acce2df27a3d63db1592b25
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-13 00:08:25 -0600

    Time: 112 ms (90.29%) | Memory: 55.1 MB (66.53%) - LeetSync

--- tree/main/DailyLeetcoding/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ tree/main/DailyLeetcoding/1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort()
+        events.sort(key=lambda e: e[0])
         minH = []          # heap of end days for started, unattended events
         i, n = 0, len(events)
         countConf = 0
