63bb151e7e5b722370ecc49391294dc3954c9a37
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-12 18:20:25 -0600

    Time: 3 ms (84.27%) | Memory: 22.7 MB (19.15%) - LeetSync

--- tree/main/DailyLeetcoding/130-surrounded-regions/surrounded-regions.py
+++ tree/main/DailyLeetcoding/130-surrounded-regions/surrounded-regions.py
@@ -1,33 +1,35 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
+        visit = set()
 
-        def capture(r, c):
-            if (r < 0 or c < 0 or r == ROWS or
-                c == COLS or board[r][c] != "O"
+        def dfs(r, c, prevHeight):
+            if ((r, c) in visit or
+                r < 0 or c < 0 or
+                r == ROWS or c == COLS or
+                board[r][c] != 'O'
             ):
                 return
-            board[r][c] = "T"
-            capture(r + 1, c)
-            capture(r - 1, c)
-            capture(r, c + 1)
-            capture(r, c - 1)
-
-        for r in range(ROWS):
-            if board[r][0] == "O":
-                capture(r, 0)
-            if board[r][COLS - 1] == "O":
-                capture(r, COLS - 1)
+            visit.add((r, c))
+            dfs(r + 1, c, board[r][c])
+            dfs(r - 1, c, board[r][c])
+            dfs(r, c + 1, board[r][c])
+            dfs(r, c - 1, board[r][c])
 
         for c in range(COLS):
-            if board[0][c] == "O":
-                capture(0, c)
-            if board[ROWS - 1][c] == "O":
-                capture(ROWS - 1, c)
+            if board[0][c] == 'O':
+                dfs(0, c, board[0][c])
+            if board[ROWS - 1][c] == 'O':
+                dfs(ROWS - 1, c, board[ROWS - 1][c])
+
+        for r in range(ROWS):
+            if board[r][0] == 'O':
+                dfs(r, 0, board[r][0])
+            if board[r][COLS - 1] == 'O':
+                dfs(r, COLS - 1, board[r][COLS - 1])
 
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
+                if board[r][c] == 'O' and (r, c) not in visit:
+                    board[r][c] = 'X'
+        
