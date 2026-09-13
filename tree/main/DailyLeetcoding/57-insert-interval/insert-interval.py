dfc1fe0d49c0ee6f013e44237aff57c163602b88
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-13 14:40:41 -0600

    Time: 3 ms (29.81%) | Memory: 21.4 MB (57.54%) - LeetSync

--- tree/main/DailyLeetcoding/57-insert-interval/insert-interval.py
+++ tree/main/DailyLeetcoding/57-insert-interval/insert-interval.py
@@ -1,50 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        if len(intervals) == 0:
-            return [newInterval]
-        ans = [] # [[1, 2], [3, 8]]
-        keepContinue = False
+        res = []
+
         for i in range(len(intervals)):
-            
-            if newInterval and intervals[i][0] < newInterval[0] and intervals[i][1] < newInterval[0]:
-                ans.append(intervals[i])
-            elif not keepContinue:
-                if intervals[i][0] < newInterval[0]:
-                    ans.append(intervals[i])
-                    ans.append(newInterval)
-                    newInterval = None  
-                elif intervals[i][0] > newInterval[0]:
-                    ans.append(newInterval)
-                    ans.append(intervals[i])
-                    newInterval = None  
-                elif intervals[i][1] < newInterval[1]:
-                    ans.append(intervals[i])
-                    ans.append(newInterval)
-                    newInterval = None  
-                else:
-                    ans.append(newInterval)
-                    ans.append(intervals[i])
-                    newInterval = None  
-                keepContinue = True
+            cur = intervals[i]
+            if cur[1] < newInterval[0]:
+                res.append(cur)
+            elif cur[0] > newInterval[1]:
+                res.append(newInterval)
+                return res + intervals[i:]
             else:
-                ans.append(intervals[i])
+                s = min(cur[0], newInterval[0])
+                e = max(cur[1], newInterval[1])
+                newInterval = [s, e]
         
-        if newInterval != None:
-            ans.append(newInterval)
-        
-        print("ans : ", ans)
-        finalAns = [ans[0]]
-        
-        for i in range(1, len(ans)):
-            if finalAns[-1][1] >= ans[i][0]:
-                finalAns[-1][1] = max(finalAns[-1][1], ans[i][1])
-            else:
-                finalAns.append(ans[i])
+        res.append(newInterval)
+        return res
 
         
-        print("ans after merging: ", finalAns)
-
-        return finalAns
-
-
-            
