18171c73bcdaa8df1d6276455ea3a8d5df1a1b60
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-14 23:13:53 -0600

    Time: 3 ms (61.30%) | Memory: 21.9 MB (5.57%) - LeetSync

--- tree/main/DailyLeetcoding/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ tree/main/DailyLeetcoding/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,6 +1,20 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        for i in range(len(cost) - 3, -1, -1):
-            cost[i] += min(cost[i + 1], cost[i + 2])
 
-        return min(cost[0], cost[1])
+        # top-down approach
+        memo = {}
+        def dfs(index):
+            if index >= len(cost):
+                return 0
+            
+            if index in memo:
+                return memo[index]
+
+            left = dfs(index + 1)
+            right = dfs(index + 2)
+
+            memo[index] = cost[index] + min(left, right)
+
+            return memo[index]
+        
+        return min(dfs(0), dfs(1))
