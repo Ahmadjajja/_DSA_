c2734844ef4d646aae3ab7a137b853a64f360f56
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-15 19:13:40 -0600

    Time: 3 ms (50.33%) | Memory: 20.4 MB (39.12%) - LeetSync

--- tree/main/DailyLeetcoding/110-balanced-binary-tree/balanced-binary-tree.py
+++ tree/main/DailyLeetcoding/110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,28 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-
-        def dfs(node, ans):
-            if not ans:
-                return [0, ans]
+    def isBalanced(self, root: TreeNode | None) -> bool:
+        def dfs(node):
             if not node:
-                return [0, ans]
-
+                return [0, True]
             
-            left = dfs(node.left, ans)
-            right = dfs(node.right, ans)
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-            if not left[1] or not right[1]:
+            if not (left[1] and right[1]) or abs(left[0] - right[0]) > 1:
                 return [0, False]
 
-            if abs(left[0] - right[0]) > 1:
-                return [0, False]
-            
-            return [max(left[0], right[0]) + 1, True]
+            return [1 + max(left[0], right[0]), True]
 
-        return dfs(root, True)[1]
-
-# tc: O(n)
-# sc: O(h)
+        return dfs(root)[1]
         
