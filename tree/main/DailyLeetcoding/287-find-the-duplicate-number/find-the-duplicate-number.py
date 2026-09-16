95d4f176e68494112850e25eb190422a72f8fa8a
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-16 10:37:29 -0600

    Time: 27 ms (54.57%) | Memory: 33.5 MB (75.39%) - LeetSync

--- tree/main/DailyLeetcoding/287-find-the-duplicate-number/find-the-duplicate-number.py
+++ tree/main/DailyLeetcoding/287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,14 +1,5 @@
-# class Solution:
-#     def findDuplicate(self, nums: List[int]) -> int:
-#         for num in nums :
-#             idx = abs(num) - 1
-#             if nums[idx] < 0 :
-#                 return abs(num)
-#             nums[idx] *= -1
-#         return -1
-
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
+    def findDuplicate(self, nums: list[int]) -> int:
         slow, fast = 0, 0
         while True:
             slow = nums[slow]
@@ -17,12 +8,11 @@ class Solution:
                 break
 
         slow2 = 0
+
         while True:
             slow = nums[slow]
             slow2 = nums[slow2]
             if slow == slow2:
                 return slow
-        
-        
-        
+
         
