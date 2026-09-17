c5e975c6ba6b80e9b4437b82b44d66e84a3ab898
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-16 23:52:59 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.56%) - LeetSync

--- tree/main/DailyLeetcoding/191-number-of-1-bits/number-of-1-bits.py
+++ tree/main/DailyLeetcoding/191-number-of-1-bits/number-of-1-bits.py
@@ -1,11 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
-
-
         while n:
-            count += (n % 2)
-            n = n >> 1
-        
+            count += n & 1   # check rightmost bit
+            n >>= 1          # shift right, drop that bit
         return count
-        
