7559dce798f2fc8a37343c4e1d6bb6b582628af6
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-17 01:11:28 -0600

    Time: 31 ms (79.93%) | Memory: 20 MB (16.51%) - LeetSync

--- tree/main/DailyLeetcoding/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ tree/main/DailyLeetcoding/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,30 +1,22 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        pf = Counter(p)
-        sf = {}
+        if len(p) > len(s):
+            return []
+
+        pCount = [0] * 26
+        sCount = [0] * 26
+
+        for ch in p:
+            pCount[ord(ch) - ord('a')] += 1
+
         res = []
+        for i in range(len(s)):
+            sCount[ord(s[i]) - ord('a')] += 1
 
-        l = 0
+            if i >= len(p):
+                sCount[ord(s[i - len(p)]) - ord('a')] -= 1
 
-        for r in range(len(s)):
-            ch = s[r]
+            if sCount == pCount:
+                res.append(i - len(p) + 1)
 
-            if ch in pf:
-                sf[ch] = sf.get(ch, 0) + 1
-                while sf[ch] > pf[ch]:
-                    sf[s[l]] -= 1
-                    l += 1
-                if (r - l + 1) == len(p):
-                    res.append(l)
-                    sf[s[l]] -= 1
-                    l += 1
-            else:
-                sf.clear()
-                l = r + 1
-        
         return res
-            
-
-
-
-        
