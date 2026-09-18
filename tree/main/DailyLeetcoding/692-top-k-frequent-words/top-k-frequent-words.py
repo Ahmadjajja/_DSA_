e6af6a80406eda6c751c5cc0b5c4df5891b5d109
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-17 21:45:03 -0600

    Time: 11 ms (6.63%) | Memory: 19.7 MB (5.61%) - LeetSync

---
+++ tree/main/DailyLeetcoding/692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,29 @@
+class Solution:
+    def topKFrequent(self, words: list[str], k: int) -> list[str]:
+        words.sort()
+        initial_freq = {}
+        for i in range(len(words)):
+            word = words[i]
+            if word in initial_freq:
+                freq, index = initial_freq[word]
+                initial_freq[word] = [freq + 1, index]
+            else:
+                initial_freq[word] = [1, i]
+        
+        print("initial_freq : ", initial_freq)
+        cur_freq = {}
+        for key, val in initial_freq.items():
+            freq, index = val
+            cur_freq[(-freq, index)] = key
+
+        print("cur_freq : ", cur_freq)
+        maxH = list(cur_freq.keys())
+        heapq.heapify(maxH)
+        res = []
+        for _ in range(k):
+            poppedElem = heapq.heappop(maxH)
+            print("poppedElem : ", poppedElem)
+            res.append(cur_freq[poppedElem])
+        return res
+
+        
