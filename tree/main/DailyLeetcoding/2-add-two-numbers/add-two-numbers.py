b4abd7fcacd6323ce9d38a0bdfac873064cc3de1
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-18 10:27:48 -0600

    Time: 18 ms (6.61%) | Memory: 19.5 MB (12.02%) - LeetSync

--- tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
+++ tree/main/DailyLeetcoding/2-add-two-numbers/add-two-numbers.py
@@ -3,25 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        dummy = ListNode(0)
-        cur = dummy
+    def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
+        dummy = ListNode(0, None)
+        l3 = dummy
+
         carry = 0
-
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            total = v1 + v2 + carry
-            carry = total // 10
-            val = total % 10
-
-            cur.next = ListNode(val)
-            cur = cur.next
-
-            if l1: l1 = l1.next
-            if l2: l2 = l2.next
-
+        while l1 or l2:
+            curSum = carry
+            if l1:
+                curSum += l1.val
+                l1 = l1.next
+            if l2:
+                curSum += l2.val
+                l2 = l2.next
+            
+            curNodVal = curSum % 10
+            print("curNodVal -> ", curNodVal)
+            carry = curSum // 10
+            print("carry -> ", carry)
+            l3.next = ListNode(curNodVal, None)
+            l3 = l3.next
+        
+        if carry > 0:
+            l3.next = ListNode(carry, None)
+        
         return dummy.next
+
+            
+        
