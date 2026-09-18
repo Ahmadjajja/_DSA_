d81372a1f4b1beba25712fee90fe243db65c22c2
Ahmad Sultan <86593662+Ahmadjajja@users.noreply.github.com>
2026-09-17 21:59:31 -0600

    Time: 3 ms (54.49%) | Memory: 19.6 MB (20.43%) - LeetSync

--- tree/main/DailyLeetcoding/692-top-k-frequent-words/top-k-frequent-words.py
+++ tree/main/DailyLeetcoding/692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,30 +1,19 @@
 class Solution:
     def topKFrequent(self, words: list[str], k: int) -> list[str]:
-        words.sort()
-        initial_freq = {}
-        for i in range(len(words)):
-            word = words[i]
-            if word in initial_freq:
-                freq, index = initial_freq[word]
-                initial_freq[word] = [freq + 1, index]
-            else:
-                initial_freq[word] = [1, i]
-        
-        cur_freq = {}
-        for key, val in initial_freq.items():
-            freq, index = val
-            cur_freq[(-freq, index)] = key
+        freq = Counter(words)
+        maxH = []
+        for key, val in freq.items():
+            maxH.append((-val, key))
 
-        maxH = list(cur_freq.keys())
         heapq.heapify(maxH)
         res = []
         for _ in range(k):
-            poppedElem = heapq.heappop(maxH)
-            res.append(cur_freq[poppedElem])
-
+            freq, val = heapq.heappop(maxH)
+            res.append(val)
         return res
 
+
         # tc -> O(nlogn) + O(n) + O(u) + O(u) + O(klogu) -> O(nlogn)
-        # sc -> O(n) + O(u) + O(k) -> O(n)
+        # sc -> O(u) + O(u) + O(k) -> O(n)
 
         
